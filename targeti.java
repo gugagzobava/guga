779c6a34b428d1e4a8ac96d15107148b46cbfc88
S <s>
2023-11-03 12:51:19 +0400

    {"types":"C","runs":9}

--- targeti.java
+++ targeti.java
@@ -5,13 +5,17 @@ import java.awt.*;
 public class targeti extends GraphicsProgram {
 	public void run() {
 		double r = 72;
-		drawCircle(getWidth() / 2 - r, getHeight() / 2 - r , 2 * r, 2 * r);
+		Color red = Color.RED;
+		Color white = Color.WHITE;
+		drawCircle(getWidth() / 2 - r, getHeight() / 2 - r , 2 * r, 2 * r, red);
 //		drawCircle();
 //		drawCircle();
 	}
 
-	private void drawCircle(double d, double e, double f, double g) {
+	private void drawCircle(double d, double e, double f, double g, Color paint) {
 		GOval circle = new GOval (d, e, f, g);
+		circle.setFilled(true);
+		circle.setFillColor(paint);
 		add(circle);
 		
 	}
