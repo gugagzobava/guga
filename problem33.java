8801aa22b94ae7feccba5c5e61fc9d5603784991
S <s>
2023-11-04 12:48:04 +0400

    {"types":"C","runs":1}

--- problem33.java
+++ problem33.java
@@ -1,4 +1,19 @@
+import java.util.Random;
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
 
-public class problem33 {
+
+public class problem33 extends ConsoleProgram {
+	public void run() {
+		int r = new Random().nextInt(150) + 50;
+		GOval oval = new GOval (getWidth() / 2 - r, getHeight() / 2 - r, 2 * r, 2 * r);
+		oval.setFilled(true);
+		add(oval);
+		
+		
+		
+		
+		
+	}
 
 }
