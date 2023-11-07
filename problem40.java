09b64bd90baad2902222a3c4aaa28c8b08a421f3
S <s>
2023-11-07 11:51:20 +0400

    {"types":"C","runs":3}

--- problem40.java
+++ problem40.java
@@ -3,8 +3,8 @@ import acm.program.GraphicsProgram;
 
 public class problem40 extends GraphicsProgram{
 	public void run() {
-		GOval ball = new GOval (100, 100, getHeight() / 2 - 50, 0);
-		add(ball);
+		GOval ball = new GOval (100, 100);
+		add(ball, 0, getHeight() / 2 - 50);
 		while (true){
 			ball.move(1,0);
 			pause(5);
