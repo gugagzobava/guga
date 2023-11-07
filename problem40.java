569c10a03a1b1867d52cc2c5cfeea4cf40bc6869
S <s>
2023-11-07 11:55:08 +0400

    {"types":"C","runs":4}

--- problem40.java
+++ problem40.java
@@ -5,9 +5,15 @@ public class problem40 extends GraphicsProgram{
 	public void run() {
 		GOval ball = new GOval (100, 100);
 		add(ball, 0, getHeight() / 2 - 50);
+		double dx = 1;
 		while (true){
-			ball.move(1,0);
+			ball.move(dx,0);
+			if (ball.getX() <= 0 || ball.getX() + ball.getWidth() / 2 <= 0){
+				dx = -dx;
+			}
+				
 			pause(5);
+			
 		}
 		
 		
