145b13bc34694e0b8c0a8f7ed86e027ad7729ad3
S <s>
2023-11-09 17:18:54 +0400

    {"types":"C","runs":2}

--- mouseevent.java
+++ mouseevent.java
@@ -18,7 +18,7 @@ public class mouseevent extends GraphicsProgram{
 		addMouseListeners();
 	}
 	public void mouseClicked (MouseEvent e) {
-		e.getY();
+//		e.getY();
 		
 		GObject obj = getElementAt(e.getX(), e.getY());
 		if (obj != null){
