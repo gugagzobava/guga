a725b25586fa09718c828ba8215c5efb9b526cef
S <s>
2023-11-12 14:35:09 +0400

    {"types":"C","runs":12}

--- problem33.java
+++ problem33.java
@@ -6,13 +6,12 @@ public class problem33 extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
 	public void run() {
-		for (int i = 0; i < 1000; i++) {
-			int d = rgen.nextInt(100, 300);
-			GOval oval = new GOval(d, d);
-			oval.setFilled(true);
-			oval.setColor(rgen.nextColor());
-			add(oval, getWidth() / 2 - d / 2, getHeight() / 2 - d / 2);
-		}
+		int d = rgen.nextInt(100, 300);
+		GOval oval = new GOval(d, d);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
+		add(oval, getWidth() / 2 - d / 2, getHeight() / 2 - d / 2);
+
 	}
 
 }
--- problem34.java
+++ problem34.java
@@ -1,4 +1,23 @@
 
-public class problem34 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class problem34 extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			int d = rgen.nextInt(100, 300);
+			GOval oval = new GOval(d, d);
+			oval.setFilled(true);
+			oval.setColor(rgen.nextColor());
+			double x = rgen.nextDouble(0, getWidth());
+			double y = rgen.nextDouble(0, getHeight());
+			add(oval, x, y);
+
+		}
+
+	}
 
 }
