aad44c1bc70681ebc110dc024cdba23532e900ea
S <s>
2023-11-12 18:31:11 +0400

    {"types":"C","runs":38}

--- problem41.java
+++ problem41.java
@@ -7,21 +7,25 @@ import acm.util.RandomGenerator;
 public class problem41 extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int d = 20;
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
-	
+
 	}
+
 	public void mouseMoved(MouseEvent e) {
 		double x = e.getX();
-        double y = e.getY();
-    	GOval oval = new GOval (x - d / 2, y - d / 2, d , d);
+		double y = e.getY();
+		GOval oval = new GOval(x - d / 2, y - d / 2, d, d);
 		oval.setFilled(true);
 		oval.setColor(rgen.nextColor());
 		add(oval);
-        
+
 	}
-	 public void mouseClicked(MouseEvent e) {
-	            removeAll(); // Clear the canvas
-	        
-	 }
+
+	public void mouseClicked(MouseEvent e) {
+		if (e.getClickCount() == 3) {
+			removeAll(); // Clear the canvas
+		}
+	}
 }
