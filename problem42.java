8f281de45c7efcee82ab7d837a6c38211c78ee17
S <s>
2023-11-12 18:51:01 +0400

    {"types":"C","runs":45}

--- problem42.java
+++ problem42.java
@@ -5,13 +5,18 @@ import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class problem42 extends GraphicsProgram{
+	 GLine section = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
 	public void run() {
 		addMouseListeners();
 	}
 	public void mousePressed(MouseEvent e){
-		 GLine section = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+		
 	        add(section);
 		
 	}
+	 public void mouseDragged(MouseEvent e) {
+	        // Update the second vertex as the mouse is dragged
+	        section.setEndPoint(e.getX(), e.getY());
+	 }
 
 }
