62ea85d87832eb32013487b01f1995f8cc21c7bd
S <s>
2023-11-12 20:04:45 +0400

    {"types":"C","runs":1}

--- problem44.java
+++ problem44.java
@@ -4,27 +4,47 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class problem44 extends GraphicsProgram {
-	private GOval oval;
-	private static final int d = 50;
+	  private GOval oval;
+	    private static final int d = 50;
 
-	public void run() {
-		addMouseListeners();
-	}
+	    public void run() {
+	        addMouseListeners();
+	    }
 
-	public void mouseClicked(MouseEvent e) {
-		oval = new GOval(d, d);
-		oval.setFilled(true);
-		add(oval, e.getX() - d / 2, e.getY() - d / 2);
-		moveOvalDown();
+	    public void mouseClicked(MouseEvent e) {
+	        oval = new GOval(d, d);
+	        oval.setFilled(true);
+	        add(oval, e.getX() - d / 2, e.getY() - d / 2);
+	        moveOvalDown();
+	    }
 
-	}
-
-	private void moveOvalDown() {
-		double i = oval.getY();
-		for (i = 0; i < getHeight() ; i++) {
-			oval.move(0, 1);
-			pause(100);
-
-		}
-	}
+	    private void moveOvalDown() {
+	        while (oval.getY() < getHeight() - d) {
+	            oval.move(0, 1);
+	            pause(100);
+	        }
+	    }
+//	private GOval oval;
+//	private static final int d = 50;
+//
+//	public void run() {
+//		addMouseListeners();
+//	}
+//
+//	public void mouseClicked(MouseEvent e) {
+//		oval = new GOval(d, d);
+//		oval.setFilled(true);
+//		add(oval, e.getX() - d / 2, e.getY() - d / 2);
+//		moveOvalDown();
+//
+//	}
+//
+//	private void moveOvalDown() {
+//		double i = oval.getY();
+//		for (i = 0; i < getHeight() ; i++) {
+//			oval.move(0, 1);
+//			pause(100);
+//
+//		}
+//	}
 }
