b80045a725f920dc9e3ab15ecb813d533eb99bfa
S <s>
2023-11-12 15:52:45 +0400

    {"types":"C","runs":69}

--- problem38.java
+++ problem38.java
@@ -6,29 +6,29 @@ public class problem38 extends ConsoleProgram {
 
 	public void run() {
 		int balance = 1000;
-		println("Your balance is " +balance);
+		println("Your balance is " + balance);
 		while (true) {
 			int n = readInt("Choose number: ");
 			if (n > 36) {
 				println("Illegal number format ");
 			} else {
 				int bid = readInt("Enter your bid: ");
-				if (bid > balance){
+				if (bid > balance) {
 					println("You don't have enough money");
-					break;
-				}
-				int x = rgen.nextInt(0, 36);
-				println("Roulette result is " + x);
-				if (n == x) {
-					println("you win");
-					balance = balance + bid;
 				} else {
-					println("you lost");
-					balance = balance - bid;
+					int x = rgen.nextInt(0, 36);
+					println("Roulette result is " + x);
+					if (n == x) {
+						println("you win");
+						balance = balance + bid;
+					} else {
+						println("you lost");
+						balance = balance - bid;
+					}
+					println("Your balance is " + balance);
 				}
-				println("Your balance is " +balance);
 			}
 		}
-	}
 
+	}
 }
