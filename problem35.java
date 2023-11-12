63121d191ccddd0c9acf52e9fdde24e854569edb
S <s>
2023-11-12 14:52:49 +0400

    {"types":"C","runs":18}

--- problem35.java
+++ problem35.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem35 {
+public class problem35 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int result = rgen.nextInt(0,1);
+		if (result == 1) {
+			println("heads");
+			} else {
+				println("tails");
+		}
+	}
+	
 
 }
