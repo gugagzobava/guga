e371ed2e915f3bf30e67eed4f29a2d5649b9aa53
S <s>
2023-11-12 17:27:09 +0400

    {"types":"C","runs":4}

--- problem39.java
+++ problem39.java
@@ -3,14 +3,15 @@ import acm.program.GraphicsProgram;
 
 public class problem39 extends GraphicsProgram {
 	private static final int r = 25;
-	public void run(){
-		GOval ball = new GOval (2 * r, 2 * r);
+
+	public void run() {
+		GOval ball = new GOval(2 * r, 2 * r);
 		ball.setFilled(true);
 		add(ball, 0, getHeight() / 2 - r);
-		while(true){
+		while (true) {
 			ball.move(1, 0);
 			pause(10);
-			if(ball.getX() + 2 * r == getWidth()){
+			if (ball.getX() + 2 * r == getWidth()) {
 				break;
 			}
 		}
