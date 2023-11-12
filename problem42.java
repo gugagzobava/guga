4ad127198e84b58fa967c9df8d127c4aaa386806
S <s>
2023-11-12 18:44:34 +0400

    {"types":"C","runs":41}

--- problem42.java
+++ problem42.java
@@ -1,17 +1,16 @@
 import java.awt.event.MouseEvent;
 
-import acm.graphics.GOval;
+import acm.graphics.GLine;
+
 import acm.program.GraphicsProgram;
 
 public class problem42 extends GraphicsProgram{
 	public void run() {
 		addMouseListeners();
 	}
-	public void mousePressed(MouseEvent e){
-		 GOval dot1 = new GOval(e.getX(), e.getY(), 5, 5);
-         add(dot1);
-         GOval dot2 = new GOval(e.getX(), e.getY(), 5, 5);
-         add(dot2);
+	public void mouseDragged(MouseEvent e){
+		 GLine section = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+	        add(section);
 		
 	}
 
