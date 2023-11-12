88ac83e3ca2b2fd55c5909c8a406b257be178527
S <s>
2023-11-12 17:14:25 +0400

    {"types":"C","runs":1}

--- problem39.java
+++ problem39.java
@@ -1,4 +1,12 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class problem39 {
+public class problem39 extends GraphicsProgram {
+	private static final int r = 25;
+	public void run(){
+		GOval ball = new GOval (2 * r, 2 * r);
+		ball.setFilled(true);
+		add(ball, 0, getHeight() / 2 - r);
+	}
 
 }
