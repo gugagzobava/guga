000969df92295bf7ac88be77dc90cc1e8119c6b2
S <s>
2023-11-12 14:26:19 +0400

    {"types":"C","runs":3}

--- problem33.java
+++ problem33.java
@@ -6,7 +6,8 @@ public class problem33 extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
 	public void run() {
-		GOval oval = new GOval(rgen.nextInt(1, 100), rgen.nextInt(1, 100));
+		int d = rgen.nextInt(1, 100);
+		GOval oval = new GOval(d, d);
 		oval.setFilled(true);
 		oval.setColor(rgen.nextColor());
 		add(oval, 200, 200);
