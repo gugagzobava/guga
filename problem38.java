5b7b2f52b72ca09897ac4832622cc3fb1c57083c
S <s>
2023-11-12 15:37:12 +0400

    {"types":"C","runs":49}

--- problem38.java
+++ problem38.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem38 {
+public class problem38 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		while (true) {
+			int balance = 1000;
+			println(balance);
+			int n = readInt("Place your bid to number: ");
+			if (n > 36) {
+				println("Illegal number format ");
+			} else {
+				int x = rgen.nextInt(0, 36);
+				println("Roulette result is " + x);
+				if (n == x) {
+					println("you win");
+				} else {
+					println("you lost");
+				}
+			}
+		}
+	}
 
 }
