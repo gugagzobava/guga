9f822c2cd062ea769b39c3cf205a486598d41a2d
S <s>
2023-11-12 15:26:43 +0400

    {"types":"C","runs":45}

--- problem37.java
+++ problem37.java
@@ -5,15 +5,16 @@ public class problem37 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
 	public void run() {
-		int n = readInt("Place your bid: ");
-		int x = rgen.nextInt(0, 36);
-		println("Roulette result is " +x);
-		if (n == x) {
-			println("you win");
-		} else {
-			println("you lost");
+		while (true) {
+			int n = readInt("Place your bid: ");
+			int x = rgen.nextInt(0, 36);
+			println("Roulette result is " + x);
+			if (n == x) {
+				println("you win");
+			} else {
+				println("you lost");
+			}
 		}
-
 	}
 
 }
