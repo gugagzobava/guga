e39502837aa8f294b5b9f5ee320258678e3d6438
S <s>
2023-11-12 18:46:18 +0400

    {"types":"C","runs":42}

--- problem42.java
+++ problem42.java
@@ -5,13 +5,35 @@ import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class problem42 extends GraphicsProgram{
-	public void run() {
-		addMouseListeners();
-	}
-	public void mouseDragged(MouseEvent e){
-		 GLine section = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+	  private double startX;
+	    private double startY;
+
+	    public void run() {
+	        addMouseListeners();
+	    }
+
+	    public void mousePressed(MouseEvent e) {
+	        startX = e.getX();
+	        startY = e.getY();
+	    }
+
+	    public void mouseDragged(MouseEvent e) {
+	        double endX = e.getX();
+	        double endY = e.getY();
+
+	        GLine section = new GLine(startX, startY, endX, endY);
 	        add(section);
-		
-	}
+
+	        startX = endX;
+	        startY = endY;
+	    }
+//	public void run() {
+//		addMouseListeners();
+//	}
+//	public void mouseDragged(MouseEvent e){
+//		 GLine section = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+//	        add(section);
+//		
+//	}
 
 }
