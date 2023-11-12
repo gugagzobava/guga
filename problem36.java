35e9c03415b15404989d4dda65af2746faa18421
S <s>
2023-11-12 15:09:06 +0400

    {"types":"C","runs":34}

--- problem36.java
+++ problem36.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem36 {
+public class problem36 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		int flips = 0;
+		int headsInRow = 0;
+		while (true) {
+			int result = rgen.nextInt(0, 1);
+			flips++;
+			if (result == 1) {
+				println("heads");
+				headsInRow++;
+				break;
+			} else {
+				println("tails");
+				headsInRow = 0;
+			}
+		}
+		println("it took " + flips + " flips to get heads");
+	}
 
 }
