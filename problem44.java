cfdf715e864c1062faccf5c9aaa802105c0f2d38
S <s>
2023-11-13 23:20:35 +0400

    {"types":"C","runs":6}

--- problem44.java
+++ problem44.java
@@ -4,25 +4,28 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class problem44 extends GraphicsProgram {
-	  private GOval oval;
-	    private static final int d = 50;
+	private GOval oval;
+	private static final int d = 50;
 
-	    public void run() {
-	        addMouseListeners();
+	public void run() {
+		addMouseListeners();
 
-	        while (oval.getY() < getHeight() - d) {
-	            oval.move(0, 1);
-	            pause(10);
-	        }
-	    }
+		while (oval.getY() < getHeight() - d) {
 
-	    public void mouseClicked(MouseEvent e) {
-	        oval = new GOval(d, d);
-	        oval.setFilled(true);
-	        add(oval, e.getX() - d / 2, e.getY() - d / 2);
-	        
-	    }
+		}
+	}
 
-	  
-	    
+	public void mouseClicked(MouseEvent e) {
+		oval = new GOval(d, d);
+		oval.setFilled(true);
+		add(oval, e.getX() - d / 2, e.getY() - d / 2);
+		oval.setLocation(e.getX() - d / 2, e.getY() - d / 2);
+		while (oval.getY() < getHeight() - d) {
+			oval.move(0, 1);
+			pause(10);
+
+		}
+
+	}
+
 }
