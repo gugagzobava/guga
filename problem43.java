7680ca73fdbc4c216977e33f883eac2aebf2776b
S <s>
2023-11-13 23:17:05 +0400

    {"types":"C","runs":3}

--- problem43.java
+++ problem43.java
@@ -7,53 +7,27 @@ import acm.util.RandomGenerator;
 public class problem43 extends GraphicsProgram{
 	private RandomGenerator rgen = RandomGenerator.getInstance();
     private GOval oval;
-    private double lastX;
-    private double lastY;
+    double d = rgen.nextDouble(50, 100);
 
     public void run() {
         addMouseListeners();
     }
 
     public void mousePressed(MouseEvent e) {
-        double d = rgen.nextDouble(50, 100);
         oval = new GOval(d, d);
         oval.setFilled(true);
         oval.setColor(rgen.nextColor());
         add(oval, e.getX(), e.getY());
 
-        lastX = e.getX();
-        lastY = e.getY();
+        
     }
 
     public void mouseDragged(MouseEvent e) {
-            double dx = e.getX() - lastX;
-            double dy = e.getY() - lastY;
-            oval.move(dx, dy);
-            lastX = e.getX();
-            lastY = e.getY();
+            double x = e.getX() - d / 2;
+            double y = e.getY() - d / 2;
+            oval.setLocation(x, y);
+           
         }
     
    
-//	private RandomGenerator rgen = RandomGenerator.getInstance();
-//	private GOval oval;
-//	public void run(){
-//		addMouseListeners();
-//	}
-//		public void mousePressed(MouseEvent e){
-//		double d = rgen.nextDouble(50, 100);
-//		oval = new GOval(d, d);
-//		oval.setFilled(true);
-//		oval.setColor(rgen.nextColor());
-//		add(oval, e.getX(), e.getY());
-//		
-//	}
-//	public void mouseDragged(MouseEvent e){
-//		
-//		oval.move(e.getX(), e.getY());
-//		
-//	}
-//	 public void mouseReleased(MouseEvent e) {
-//	        oval = null;
-//	    }
-
 }
