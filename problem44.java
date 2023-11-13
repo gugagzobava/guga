96f834cb0011f796a1975117ae274a4bade84fd1
S <s>
2023-11-13 23:17:43 +0400

    {"types":"C","runs":4}

--- problem44.java
+++ problem44.java
@@ -25,27 +25,4 @@ public class problem44 extends GraphicsProgram {
 
 	  
 	    
-//	private GOval oval;
-//	private static final int d = 50;
-//
-//	public void run() {
-//		addMouseListeners();
-//	}
-//
-//	public void mouseClicked(MouseEvent e) {
-//		oval = new GOval(d, d);
-//		oval.setFilled(true);
-//		add(oval, e.getX() - d / 2, e.getY() - d / 2);
-//		moveOvalDown();
-//
-//	}
-//
-//	private void moveOvalDown() {
-//		double i = oval.getY();
-//		for (i = 0; i < getHeight() ; i++) {
-//			oval.move(0, 1);
-//			pause(100);
-//
-//		}
-//	}
 }
