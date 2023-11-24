f6ce0fb55453d9b35b49eb2cf717dafdf4cf6fb0
S <s>
2023-11-24 12:51:03 +0400

    {"types":"C","runs":7}

--- deletecracters.java
+++ deletecracters.java
@@ -3,20 +3,20 @@ import acm.program.ConsoleProgram;
 public class deletecracters extends ConsoleProgram {
 	public void run(){
 		String s = readLine();
-//		println(deleteCharacters(s));
+		println(deleteCharacters(s));
 		String a = "hello";
 		String b = "hell";
 	}
-//	private String deleteCharacters(String string) {
-//		String result = "";
-//		for (int i = 0; i < string.length(); i++) {
-//			if (i == 0 || string.charAt(i) != string.charAt(i-1)){
-//				result += string.charAt(i);
-//			}
-//		}
-//	
-//		return result;
-//	}
+	private String deleteCharacters(String string) {
+		String result = "";
+		for (int i = 0; i < string.length(); i++) {
+			if (i == 0 || string.charAt(i) != string.charAt(i-1)){
+				result += string.charAt(i);
+			}
+		}
+	
+		return result;
+	}
 	private boolean contains(String a, String b) {
 		for(int i = 0; i < a.length() - b.length(); i++) {
 			if(b.equals(a.substring(i, i + b.length())))	{
