abfd02ccb66b6e65a96f234b926132cc1fd54a3f
S <s>
2023-11-24 12:48:20 +0400

    {"types":"C","runs":3}

--- deletecracters.java
+++ deletecracters.java
@@ -15,5 +15,13 @@ public class deletecracters extends ConsoleProgram {
 	
 		return result;
 	}
+	private boolean contains(String a, String b) {
+		for(int i = 0; i < a.length() - b.length(); i++) {
+			if(b.equals(a.substring(i, i + b.length())))	{
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
