78b8073715dfda59649a5e6058ded25a3ee50a39
S <s>
2023-11-24 13:10:03 +0400

    {"types":"C","runs":8}

--- deletecracters.java
+++ deletecracters.java
@@ -1,29 +1,22 @@
 import acm.program.ConsoleProgram;
 
 public class deletecracters extends ConsoleProgram {
-	public void run(){
-		String s = readLine();
-		println(deleteCharacters(s));
-		String a = "hello";
-		String b = "hell";
-	}
-	private String deleteCharacters(String string) {
-		String result = "";
-		for (int i = 0; i < string.length(); i++) {
-			if (i == 0 || string.charAt(i) != string.charAt(i-1)){
-				result += string.charAt(i);
-			}
-		}
-	
-		return result;
-	}
-	private boolean contains(String a, String b) {
-		for(int i = 0; i < a.length() - b.length(); i++) {
-			if(b.equals(a.substring(i, i + b.length())))	{
-				return true;
-			}
-		}
-		return false;
+	public void run() {
+		
+
+		String a = "gghhooooeeepploll";
+		String b = "hello";
 	}
 
+	private boolean contains(String a, String b) {
+		int prevIndex = 0;
+		for (int i = 0; i < b.length(); i++) {
+			prevIndex = a.indexOf(b.charAt(i), prevIndex) + 1;
+			if (prevIndex == 0) {
+				return false;
+			}
+			return true;
+
+		}
+	}
 }
