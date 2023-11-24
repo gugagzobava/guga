1146bfe66a7455dbf132354a8dc28a94ba86328b
S <s>
2023-11-24 12:49:42 +0400

    {"types":"C","runs":6}

--- deletecracters.java
+++ deletecracters.java
@@ -7,16 +7,16 @@ public class deletecracters extends ConsoleProgram {
 		String a = "hello";
 		String b = "hell";
 	}
-	private String deleteCharacters(String string) {
-		String result = "";
-		for (int i = 0; i < string.length(); i++) {
-			if (i == 0 || string.charAt(i) != string.charAt(i-1)){
-				result += string.charAt(i);
-			}
-		}
-	
-		return result;
-	}
+//	private String deleteCharacters(String string) {
+//		String result = "";
+//		for (int i = 0; i < string.length(); i++) {
+//			if (i == 0 || string.charAt(i) != string.charAt(i-1)){
+//				result += string.charAt(i);
+//			}
+//		}
+//	
+//		return result;
+//	}
 	private boolean contains(String a, String b) {
 		for(int i = 0; i < a.length() - b.length(); i++) {
 			if(b.equals(a.substring(i, i + b.length())))	{
