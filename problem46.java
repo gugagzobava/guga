ecb414c712edd21c638a3090a9d5f72edd515f2c
S <s>
2023-11-25 13:33:23 +0400

    {"types":"ACR","runs":1}

--- problem46.java
+++ problem46.java
@@ -30,26 +30,5 @@ public class problem46 extends ConsoleProgram {
 		}
 		return count;
 	}
-//		String text = readLine("Enter text: ");
-//		int maxCount = 0;
-//		char maxChar;
-//		for (int i = 0; i < text.length(); i++) {
-//			int numOcc = numOccurences(text, text.charAt(i));
-//			if (numOcc > maxCount){
-//				maxCount = numOcc;
-//				maxChar = text.charAt(i);
-//			}
-//		}
-//		println("max char: " + maxChar);
-//	}
-//
-//	private int numOccurences(String text, char symbol) {
-//		int numOccurences = 0;
-//		for (int i = 0; i < text.length(); i++) {
-//			if (text.charAt(i) == symbol) {
-//				numOccurences++;
-//			}
-//		}
-//		return numOccurences;
-//	}
 }
+
