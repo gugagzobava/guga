bd599a58363a4c9a733466273991ed77797ff5b6
S <s>
2023-11-25 13:57:49 +0400

    {"types":"C","runs":6}

--- problem48.java
+++ problem48.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class problem48 {
+public class problem48 extends ConsoleProgram{
+	public void run() {
+		String s = readLine("Enter string: ");
+		printSymbolStatistics(s);
+	}
+
+	private void printSymbolStatistics(String s) {
+		for(int i = 0; i < 26; i++) {
+			char next = (char) ('a' + i);
+			countSymbol(s, next);
+			println("Symbol '" + next + "' occurs " + countSymbol + " times.");
+		}
+		
+	}
+	private int countSymbol(String s, char ch){
+		int count = 0;
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) == ch){
+				count++;
+			}
+		}
+		return count;
+	}
 
 }
