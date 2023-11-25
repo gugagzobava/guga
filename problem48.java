e25ee213bde78b1785a9e2fc5f64005a4c68f7ea
S <s>
2023-11-25 14:00:50 +0400

    {"types":"C","runs":11}

--- problem48.java
+++ problem48.java
@@ -8,16 +8,16 @@ public class problem48 extends ConsoleProgram{
 
 	private void printSymbolStatistics(String s) {
 		for(int i = 0; i < 26; i++) {
-			char next = (char) ('a' + i);
-			int N = countSymbol(s, next);
-			println("Symbol '" + next + "' occurs " + N + " times.");
+			char ch = (char) ('a' + i);
+			int N = countSymbol(s, ch);
+			println("Symbol '" + ch + "' occurs " + N + " times.");
 		}
 		
 	}
-	private int countSymbol(String s, char next){
+	private int countSymbol(String s, char ch){
 		int count = 0;
 		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(i) == next){
+			if(s.charAt(i) == ch){
 				count++;
 			}
 		}
