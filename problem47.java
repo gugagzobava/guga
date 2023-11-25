474b6c4630b9a86189949968c72e8476efe4084b
S <s>
2023-11-25 13:38:20 +0400

    {"types":"C","runs":2}

--- problem47.java
+++ problem47.java
@@ -1,5 +1,9 @@
+import acm.program.ConsoleProgram;
 
-
-public class problem47 {
+public class problem47 extends ConsoleProgram {
+	public void run() {
+		String s = readLine("enter string: ");
+		println("isPalindrome");
+	}
 
 }
