ff213f3002550827f589a08a9d107879b3ad4ac7
S <s>
2023-11-25 14:22:49 +0400

    {"types":"C","runs":22}

--- problem49.java
+++ problem49.java
@@ -11,7 +11,7 @@ public class problem49 extends ConsoleProgram {
 	private int StringToInt(String s) {
 		int result = 0;
 		for(int i = 0; i < s.length(); i++){
-			int n = s.charAt(i) - '0';
+			int n = s.charAt(i) - '1';
 			result = result * 10 + n;
 		}
 		return result;
