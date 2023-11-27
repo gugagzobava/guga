9aac1acb1915102b02ebefeb055317fa9a9e56a5
S <s>
2023-11-27 17:22:53 +0400

    {"types":"C","runs":32}

--- midterm2021_3_texili.java
+++ midterm2021_3_texili.java
@@ -1,4 +1,23 @@
+import java.awt.event.MouseEvent;
 
-public class midterm2021_3_texili {
+import acm.graphics.GLine;
+import acm.graphics.GPoint;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
+public class midterm2021_3_texili extends GraphicsProgram {
+	private GPoint lastPoint;
+	private GLine line;
+
+	public void run() {
+		addMouseListeners();
+		lastPoint = new GPoint(0, 0);
+	}
+
+	public void mousePressed(MouseEvent e) {
+		line = new GLine(lastPoint.getX(), lastPoint.getY(), e.getX(), e.getY());
+		add(line);
+		lastPoint = new GPoint(e.getX(), e.getY());
+
+	}
 }
