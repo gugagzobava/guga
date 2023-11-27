d7b385094221d576f13eebb7d73d5cb927069419
S <s>
2023-11-27 18:59:08 +0400

    {"types":"C","runs":8}

--- midterm2019_cimcima.java
+++ midterm2019_cimcima.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -9,45 +10,57 @@ public class midterm2019_cimcima extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int d = 50;
 	private GOval ball;
+	private GObject clickedObject = null;
 
 	public void run() {
 		addMouseListeners();
-
+		while (true) {
+			if (clickedObject != null) {
+				while (clickedObject.getColor() != Color.GREEN) {
+					Color color = flicking();
+					clickedObject.setColor(color);
+					pause(1000);
+				}
+			}
+		}
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		if (ball != null) {
-			remove(ball);
-		}
+		GObject object = getElementAt(e.getX(), e.getY());
+		if(object == null){
 		ball = new GOval(e.getX() - d / 2, e.getY() - d / 2, d, d);
 		add(ball);
 		ball.setFilled(true);
-		ball.setColor(rgen.nextColor());
-		flicker();
+		ball.setColor(flicking());
 		
-		
-	}
-
-	private void flicker() {
-		while (true) {
-			int x = rgen.nextInt(1, 6);
-			if (x == 1) {
-				ball.setColor(Color.BLACK);
-			}
-			if (x == 2) {
-				ball.setColor(Color.RED);
-			}
-			if (x == 3) {
-				ball.setColor(Color.BLUE);
-			}
-			if (x == 4) {
-				ball.setColor(Color.YELLOW);
-			}
-			if (x == 5) {
-				ball.setColor(Color.GREEN);
-			}
-			pause(1000);
+		} else {
+			clickedObject = object;
 		}
+		
 
 	}
+
+	private Color flicking() {
+		int x = rgen.nextInt(5);
+		if (x == 1) {
+			ball.setColor(Color.BLACK);
+		}
+		if (x == 2) {
+			ball.setColor(Color.RED);
+		}
+		if (x == 3) {
+			ball.setColor(Color.BLUE);
+		}
+		if (x == 4) {
+			ball.setColor(Color.YELLOW);
+		}
+		if (x == 5) {
+			ball.setColor(Color.GREEN);
+		}
+	
+	
+		return null;
+	}
+
+		
 }
