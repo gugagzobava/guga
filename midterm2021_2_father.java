0e959da84b70bf37548fa71d55655fb881a6c24c
S <s>
2023-11-27 17:09:24 +0400

    {"types":"C","runs":30}

--- midterm2021_2_father.java
+++ midterm2021_2_father.java
@@ -12,7 +12,7 @@ public class midterm2021_2_father extends ConsoleProgram {
 			for (int j = i + 1; j < luke.length() + 1; j++) {
 				String sameDNA = luke.substring(i, j);
 				if (anakin.indexOf(sameDNA) != -1) {
-					if (sameDNA.length() >= anakin.length()) {
+					if (sameDNA.length() >= anakin.length() / 2) {
 						return true;
 					}
 				}
