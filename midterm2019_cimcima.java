bfef82531fa2fdd616fefcdb668a818888212eb5
S <s>
2023-11-27 18:01:54 +0400

    {"types":"C","runs":46}

--- midterm2019_cimcima.java
+++ midterm2019_cimcima.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
@@ -17,6 +18,25 @@ public class midterm2019_cimcima extends GraphicsProgram{
 		add(ball);
 		ball.setFilled(true);
 		ball.setColor(rgen.nextColor());
+		int x = rgen.nextInt(1,5);
+		while(ball.getColor() != Color.GREEN){
+	
+			if (x == 1){
+				ball.setColor(Color.BLACK);
+			}
+			if(x == 2){
+				ball.setColor(Color.RED);
+			}
+			if(x == 3){
+				ball.setColor(Color.BLUE);
+			}
+			if(x == 4){
+				ball.setColor(Color.YELLOW);
+			}
+			if(x == 5){
+				ball.setColor(Color.GREEN);
+			}
+		}
 	}
 
 }
