8e16bd31debf4f70070549299071c6419556c900
S <s>
2023-11-27 18:22:16 +0400

    {"types":"C","runs":5}

--- midterm2019_cimcima.java
+++ midterm2019_cimcima.java
@@ -24,26 +24,49 @@ public class midterm2019_cimcima extends GraphicsProgram {
 		ball.setFilled(true);
 		ball.setColor(rgen.nextColor());
 		// int x = rgen.nextInt(1,5);
-		while (true) {
-			int x = rgen.nextInt(1, 5);
-			if (x == 1) {
-				ball.setColor(Color.BLACK);
-			}
-			if (x == 2) {
-				ball.setColor(Color.RED);
-			}
-			if (x == 3) {
-				ball.setColor(Color.BLUE);
-			}
-			if (x == 4) {
-				ball.setColor(Color.YELLOW);
-			}
-			if (x == 5) {
-				ball.setColor(Color.GREEN);
-break;
-			}
-			pause(1000);
-		}
-
+		 for (int i = 0; i < 5; i++) {
+	            int x = rgen.nextInt(1, 6); // Change to 6 to include 5 as a possibility
+	            switch (x) {
+	                case 1:
+	                    ball.setColor(Color.BLACK);
+	                    break;
+	                case 2:
+	                    ball.setColor(Color.RED);
+	                    break;
+	                case 3:
+	                    ball.setColor(Color.BLUE);
+	                    break;
+	                case 4:
+	                    ball.setColor(Color.YELLOW);
+	                    break;
+	                case 5:
+	                    ball.setColor(Color.GREEN);
+	                    break;
+	            }
+	            pause(1000);
+	        }
+	    }
 	}
-}
+//		while (true) {
+//			int x = rgen.nextInt(1, 5);
+//			if (x == 1) {
+//				ball.setColor(Color.BLACK);
+//			}
+//			if (x == 2) {
+//				ball.setColor(Color.RED);
+//			}
+//			if (x == 3) {
+//				ball.setColor(Color.BLUE);
+//			}
+//			if (x == 4) {
+//				ball.setColor(Color.YELLOW);
+//			}
+//			if (x == 5) {
+//				ball.setColor(Color.GREEN);
+//break;
+//			}
+//			pause(1000);
+//		}
+//
+//	}
+//}
