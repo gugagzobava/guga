14e1da30a906415ec95d53e55ac36efb66fc104a
S <s>
2023-11-27 16:36:31 +0400

    {"types":"C","runs":11}

--- midterm2021_1_log.java
+++ midterm2021_1_log.java
@@ -4,7 +4,7 @@ public class midterm2021_1_log extends ConsoleProgram {
 	public void run() {
 		int n = readInt("enter base: ");
 		int m = readInt("enter argument: ");
-		int result = m / n;
+		int result = m;
 		int answer = 1;
 		if(result != 1){
 			result = result / n;
