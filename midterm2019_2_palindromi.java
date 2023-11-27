ef391aa8ffb1446f93bc50e71e8433a09fb58e98
S <s>
2023-11-27 19:21:19 +0400

    {"types":"C","runs":8}

--- midterm2019_2_palindromi.java
+++ midterm2019_2_palindromi.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2019_2_palindromi {
+public class midterm2019_2_palindromi extends ConsoleProgram {
+	public void run(){
+		int n = readInt("enter number: ");
+		int result = 0;
+		while(n != 0){
+			int m = n % 10;
+			n /= 10;
+			result = result * 10 + m;
+		}
+	}
+	
 
 }
