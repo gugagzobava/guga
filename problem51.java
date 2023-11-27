400fbf7063a4b3dfbecc26fe9aeea12fb85bed42
S <s>
2023-11-27 22:22:22 +0400

    {"types":"C","runs":18}

--- problem51.java
+++ problem51.java
@@ -1,29 +1,24 @@
 import acm.program.ConsoleProgram;
 
-public class problem51 extends ConsoleProgram{
-	public void run(){
+public class problem51 extends ConsoleProgram {
+	public void run() {
 		String s = readLine("enter string; ");
 		removeDup(s);
-		
+
 	}
 
 	private void removeDup(String s) {
-		 String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-           
-            for (int j = 0; j < result.length(); j++) {
-             //   if (currentChar == result.charAt(j)) {
-                	 if (currentChar != result.charAt(j)) {
-                         result += currentChar;
-                     }
-                }
-            }
-        println(result);
-           
-        }
-       
-		
-	//}
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			char currentChar = s.charAt(i);
+
+			if (result.indexOf(currentChar) == -1) {
+				result += currentChar;
+			}
+		}
+
+		println(result);
+
+	}
 
 }
