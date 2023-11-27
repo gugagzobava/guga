56c52e662b80d4447e835ce6800d2a572ceffc53
S <s>
2023-11-27 17:05:12 +0400

    {"types":"C","runs":23}

--- midterm2021_2_father.java
+++ midterm2021_2_father.java
@@ -1,18 +1,20 @@
 import acm.program.ConsoleProgram;
 
-public class midterm2021_2_father extends ConsoleProgram{
-	public void run(){
+public class midterm2021_2_father extends ConsoleProgram {
+	public void run() {
 		String Luke = "ATTGATTT";
-		String Anakin = "TG";
+		String Anakin = "TGATT";
 		println(amIYourFatherLuke(Luke, Anakin));
 	}
 
 	private boolean amIYourFatherLuke(String luke, String anakin) {
-		for(int i = 0; i < luke.length(); i++){
-			for(int j = i + 1; j < luke.length() + 1; j++){
+		for (int i = 0; i < luke.length(); i++) {
+			for (int j = i + 1; j < luke.length() + 1; j++) {
 				String sameDNA = luke.substring(i, j);
-				if(anakin.indexOf(sameDNA) != -1 && sameDNA.length() >= anakin.length()){
-					return true;
+				if (anakin.indexOf(sameDNA) != -1) {
+					if (sameDNA.length() >= anakin.length()) {
+						return true;
+					}
 				}
 			}
 		}
