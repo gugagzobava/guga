6d7c06dfd69adb30ea6a05e59297d9332ae415c0
S <s>
2023-11-27 16:30:05 +0400

    {"types":"C","runs":1}

--- midterm2021_1_log.java
+++ midterm2021_1_log.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2021_1_log {
+public class midterm2021_1_log extends ConsoleProgram {
+	public void run() {
+		int n = readInt("enter base: ");
+		int m = readInt("enter argument: ");
+		int result = m / n;
+		println("log(" + n + " ," + m + ") = " + result);
+	}
 
 }
