086c296d7d8fbb209dc509da79f3ed4657ac3753
S <s>
2023-11-27 21:37:47 +0400

    {"types":"C","runs":6}

--- skyisblue.java
+++ skyisblue.java
@@ -5,7 +5,7 @@ import acm.program.ConsoleProgram;
 public class skyisblue extends ConsoleProgram{
 	public void run(){
 		String str = "the sky is blue";
-		StringTokenizer tokenizer = new StringTokenizer(str, "");
+		StringTokenizer tokenizer = new StringTokenizer(str, " ");
 		while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
             print(token + " ");
