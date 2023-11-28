7a2c87c065cfd75b07ef23badc3680bcbdce056a
S <s>
2023-11-29 00:40:47 +0400

    {"types":"C","runs":27}

--- midterm_practice_4.java
+++ midterm_practice_4.java
@@ -1,4 +1,31 @@
+import java.util.StringTokenizer;
 
-public class midterm_practice_4 {
+import acm.program.ConsoleProgram;
 
-}
+public class midterm_practice_4 extends ConsoleProgram{
+	public void run(){
+		
+			String s = readLine();
+			println(reverseWords(s));
+		}
+
+		private String reverseWords(String s) {
+			String result = "";
+			StringTokenizer tokenizer = new StringTokenizer(s);
+			while (tokenizer.hasMoreTokens()) {
+				String word = tokenizer.nextToken();
+				  result += reverse(word) + " ";
+			}
+			return result;
+	}
+
+		private String reverse(String word) {
+			 String reversed = "";
+		        for (int i = word.length() - 1; i >= 0; i--) {
+		            reversed += word.charAt(i);
+		        }
+		        return reversed;
+		    }
+		}
+
+
