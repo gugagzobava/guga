5d907e32672c3f8ca6383dc3d2457e2f771b8e32
S <s>
2023-11-28 23:27:52 +0400

    {"types":"C","runs":15}

--- midterm_practice_2.java
+++ midterm_practice_2.java
@@ -3,20 +3,16 @@ import acm.program.ConsoleProgram;
 public class midterm_practice_2 extends ConsoleProgram {
 	public void run() {
 		String s = readLine();
-		if (containsSymbol(s)) {
-			println("Error: String contains symbols. Please enter only letters.");
-		} else {
-			s = s.toUpperCase();
-			println(isIncreasing(s));
-		}
+		
+		s = s.toUpperCase();
+		println(isIncreasing(s));
 	}
 
 	private boolean isIncreasing(String s) {
 		char lastChar = 'A';
 
 		for (int i = 0; i < s.length(); i++) {
-			char currentChar = Character.toUpperCase(s.charAt(i));
-			//char currentChar = s.charAt(i);
+			char currentChar = s.charAt(i);
 			if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
 				if (currentChar <= lastChar) {
 					return false;
@@ -29,16 +25,5 @@ public class midterm_practice_2 extends ConsoleProgram {
 		return true;
 
 	}
-
-	private boolean containsSymbol(String s) {
-		for (int i = 0; i < s.length(); i++) {
-			char currentChar = s.charAt(i);
-			if (!(currentChar >= 'A' && currentChar <= 'Z')) {
-				return true; // Found a symbol
-			}
-		}
-		return false; // No symbols found
-	}
-	
 
 }
