491360c5ea4c988d82a0afc6e226f0263252d051
S <s>
2023-11-28 12:04:18 +0400

    {"types":"C","runs":4}

--- oddEven.java
+++ oddEven.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class oddEven {
+public class oddEven extends ConsoleProgram{
+	public void run(){
+		int num = readInt();
+		
+		int result = 0;
+		while(num != 0){
+			int m = num % 10;
+			
+		}
+	}
+	private boolean isAlternating(int num){
+		int last = num %10;
+		num /=10;
+		
+	}
 
 }
