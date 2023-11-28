7f36eb9952e22f381557850319ef8d18a31414ed
S <s>
2023-11-29 00:28:51 +0400

    {"types":"C","runs":26}

--- midterm_practice_3.java
+++ midterm_practice_3.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
 public class midterm_practice_3 extends ConsoleProgram {
-	public void run(){
+	public void run() {
 		String s = readLine();
 		println(upperLower(s));
 	}
@@ -12,21 +12,14 @@ public class midterm_practice_3 extends ConsoleProgram {
 		String result = "";
 		StringTokenizer tokenizer = new StringTokenizer(s);
 
-        while (tokenizer.hasMoreTokens()) {
-            String word = tokenizer.nextToken();
-            char firstChar = Character.toUpperCase(word.charAt(0));
-            String restOfWord = word.substring(1).toLowerCase();
+		while (tokenizer.hasMoreTokens()) {
+			String word = tokenizer.nextToken();
+			char firstChar = Character.toUpperCase(word.charAt(0));
+			String restOfWord = word.substring(1).toLowerCase();
 
-            result += firstChar + restOfWord + " ";
-        }
-	 
-//		for(int i = 0; i < s.length(); i++){
-//			char currentChar = s.charAt(i);
-//			if(currentChar == ' '){
-//				Character.toUpperCase(s.charAt(i+1));
-//			}
-//			result += currentChar;
-//		}
+			result += firstChar + restOfWord + " ";
+		}
+
 		return result;
 	}
 
