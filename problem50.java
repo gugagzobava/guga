c9ab220f9ef7b17bbe3c582ba25964e3335a80a2
S <s>
2023-11-28 17:14:11 +0400

    {"types":"C","runs":1}

--- problem50.java
+++ problem50.java
@@ -4,9 +4,13 @@ import acm.program.ConsoleProgram;
 
 public class problem50 extends ConsoleProgram {
 	public void run() {
+		int count = 0;
 		String str = readLine("Enter text: ");
-		StringTokenizer tokenizer = new StringTokenizer(str, "o");
-		int wordcounts = tokenizer.countTokens();
-		println(wordcounts);
+		StringTokenizer tokenizer = new StringTokenizer(str, " ");
+		while(tokenizer.hasMoreTokens()){
+		tokenizer.nextToken();
+		count++;
+		}
+		println(count);
 	}
 }
