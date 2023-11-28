e8c37a107cc2b40fc671d0c66bac734053d6c55d
S <s>
2023-11-28 23:47:38 +0400

    {"types":"C","runs":18}

--- midterm_practice_3.java
+++ midterm_practice_3.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class midterm_practice_3 {
+public class midterm_practice_3 extends ConsoleProgram {
+	public void run(){
+		String s = readLine();
+		upperLower(s);
+	}
+
+	private String upperLower(String s) {
+		String result = " ";
+		for(int i = 0; i < s.length(); i++){
+			char currentChar = s.charAt(i);
+			if(currentChar == ' '){
+				String x = ("" +s.charAt(i+1));
+				x = x.toUpperCase();
+			}
+			result += currentChar;
+		}
+		return result;
+	}
 
 }
