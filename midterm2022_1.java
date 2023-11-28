0f9e1cb5101c2476f78c6d724d872bd639d650f6
S <s>
2023-11-28 11:20:03 +0400

    {"types":"C","runs":0}

--- midterm2022_1.java
+++ midterm2022_1.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2022_1 {
+public class midterm2022_1 extends ConsoleProgram(){
+	public void run(){
+		manipulate(a, b);
+	}
+	private boolean manipulate(String a, String b){
+		
+	}
 
 }
