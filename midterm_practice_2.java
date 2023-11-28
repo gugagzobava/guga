673303d10d6fc61174301959c8b8e5b8ee64cd42
S <s>
2023-11-28 19:53:07 +0400

    {"types":"C","runs":10}

--- midterm_practice_2.java
+++ midterm_practice_2.java
@@ -8,20 +8,27 @@ public class midterm_practice_2 extends ConsoleProgram{
 		println(isIncreasing(s));
 	}
 
-	private void check(String s) {
-		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(i) > 'Z' || s.charAt(i) < 'A'){
-				println("error");
-			
-			}
-		}
-	}
+//	private void check(String s) {
+//		String result = "";
+//		for(int i = 0; i < s.length(); i++){
+//			if(s.charAt(i) > 'Z' || s.charAt(i) < 'A'){
+//				
+//			
+//			}
+//		}
+//	}
 
 	private boolean isIncreasing(String s) {
+		int count = 0;
 		for(int i = 0; i < s.length() - 1; i++){
+			if(s.charAt(i) > 'Z' || s.charAt(i) < 'A'){
+				count++;
+			}
 			if(s.charAt(i) > s.charAt(i + 1)){
 				return false;
 			}
+		} if(count != 0){
+			println("error");
 		}
 		return true;
 		
