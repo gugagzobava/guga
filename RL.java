6db317b5aca2f19075b3d77842b7de80f46296b1
S <s>
2023-11-28 15:05:50 +0400

    {"types":"C","runs":0}

--- RL.java
+++ RL.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class RL {
+public class RL extends ConsoleProgram {
+	public void run(){
+		String s = "RLRRLRLLLRRLLR";
+		countEqual(s){
+			
+			
+		}
+	}
+
+	private int countEqual(String s) {
+		countR = 0;
+		countL = 0;
+		for(int i = 0; i < s.length(); i++ ){
+			if(s.charAt(i) == 'R'){
+				countR++;
+			}
+		}
+		
+		
+	}
 
 }
