64d5ea2236bee92f08f59e7dc5814ceaff209cf4
S <s>
2023-11-28 18:03:59 +0400

    {"types":"C","runs":2}

--- midterm_practice_2.java
+++ midterm_practice_2.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class midterm_practice_2 extends ConsoleProgram{
 	public void run(){
 		String s = readLine();
-		isIncreasing(s);
+		println(isIncreasing(s));
 	}
 
 	private boolean isIncreasing(String s) {
