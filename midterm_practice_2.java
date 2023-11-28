ebeac926fadf9935b99889775bce5093e0fd63cc
S <s>
2023-11-28 23:18:35 +0400

    {"types":"C","runs":4}

--- midterm_practice_2.java
+++ midterm_practice_2.java
@@ -1,31 +1,28 @@
 import acm.program.ConsoleProgram;
 
-public class midterm_practice_2 extends ConsoleProgram{
-	public void run(){
+public class midterm_practice_2 extends ConsoleProgram {
+	public void run() {
 		String s = readLine();
 		s = s.toUpperCase();
 		println(isIncreasing(s));
 	}
 
-
 	private boolean isIncreasing(String s) {
-		char b;
-		char c = 'a';
-		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(i) > 'A' && s.charAt(i) < 'Z'){
-				b = c;
-				c = s.charAt(i);
-				if(b>=c){
+		char lastChar = 'A';
+
+		for (int i = 0; i < s.length(); i++) {
+			char currentChar = s.charAt(i);
+			if (s.charAt(i) > 'A' && s.charAt(i) < 'Z') {
+				if (currentChar <= lastChar) {
 					return false;
 				}
-				
+				lastChar = currentChar;
+
 			}
-		
-		} 
+
+		}
 		return true;
-		
-		
-		
+
 	}
 
 }
