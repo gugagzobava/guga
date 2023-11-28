246a6bc1c70e334a6c3f8c7e288251cf5fc09e2a
S <s>
2023-11-28 11:28:53 +0400

    {"types":"C","runs":1}

--- midterm2022_1.java
+++ midterm2022_1.java
@@ -1,11 +1,37 @@
 import acm.program.ConsoleProgram;
 
-public class midterm2022_1 extends ConsoleProgram(){
-	public void run(){
-		manipulate(a, b);
+public class midterm2022_1 extends ConsoleProgram {
+	public void run() {
+		String a = "lmao";
+		String b = "nice";
+		println(manipulate(a, b));
 	}
-	private boolean manipulate(String a, String b){
-		
+
+	private boolean manipulate(String a, String b) {
+		for (int i = 1; i < a.length(); i++) {
+			String left = a.substring(0, i);
+			String right = a.substring(i);
+			for (char x = 0; x < 256; x++) {
+				for (char y = 0; y < 256; y++) {
+					String s = deleteCharacter(left, x) + deleteCharacter(right, y);
+					if (s.equals(b)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+
+	}
+	private String deleteCharacter(String s, char c){
+		String result = "";
+		for(int i = 0; i<s.length(); i++){
+			if(s.charAt(i) != c){
+				result += s.charAt(i);
+				
+			}
+		}
+		return result;
 	}
 
 }
