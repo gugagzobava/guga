d000a0c990ce60ece7d786e0b95aec2ac258fbe1
S <s>
2023-11-28 11:52:02 +0400

    {"types":"C","runs":2}

--- midterm2022_1_differentSymbols.java
+++ midterm2022_1_differentSymbols.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2022_1_differentSymbols {
+public class midterm2022_1_differentSymbols extends ConsoleProgram {
+	private String end = "END";
+	public void run() {
+		String result = "";
+		int maxCount = 0;
+		while (true) {
+			String s = readLine("");
+
+			if (s.equals(end)) {
+				break;
+			}
+			int num = countCharacters(s);
+			if (num > maxCount){
+				maxCount = num;
+				result = s;
+		}
+		
+		}
+
+	}
+
+	private int countCharacters(String s) {
+		String result = "";
+		for (int i = 0; i < s.length(); i++){
+			if(!result.contains("" + s.charAt(i))){
+				result += s.charAt(i);
+			}
+		}
+		return result.length();
+	}
 
 }
