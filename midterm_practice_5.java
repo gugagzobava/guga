c8dcd01ae48be63f25ba7e028ceed8dafb4f1141
S <s>
2023-11-30 01:18:42 +0400

    {"types":"C","runs":27}

--- midterm_practice_5.java
+++ midterm_practice_5.java
@@ -2,54 +2,40 @@ import acm.program.ConsoleProgram;
 
 public class midterm_practice_5 extends ConsoleProgram{
 	public void run(){
-//		
-		  String a = "mama saa";
-	        String b = "mimaiam";
-	        println(anagrams(a, b));
-	    }
+		String a = "mama mia";
+		String b = "mimaiam";
+		println(anagrams(a, b));
+		
+			
+		
+	}
 
-	    private boolean anagrams(String a, String b) {
-	        a = a.toLowerCase();
-	        b = b.toLowerCase();
-	        int difference = 0;
-
-	        for (int i = 0; i < a.length(); i++) {
-	            if (a.charAt(i) != b.charAt(i)) {
-	                difference++;
-	                if (difference > 1) {
+	private boolean anagrams(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+		int difference = 0;
+		for(int i = 0; i < a.length(); i++){
+			char current = a.charAt(i);
+			 if (Character.isLetter(current)) {
+	                int occurrencesA = countOccurrences(a, current);
+	                int occurrencesB = countOccurrences(b, current);
+	                if (occurrencesA != occurrencesB) {
 	                    return false;
 	                }
 	            }
-	            return true;
-	        }
+		}
+		return true;
+	}
 
-	           
-	        
-
-	       return checkAnagrams(a, b);
-	    }
-
-	    private boolean checkAnagrams(String a, String b) {
-	        for (char c = 'a'; c <= 'z'; c++) {
-	            int occurrencesA = countOccurrences(a, c);
-	            int occurrencesB = countOccurrences(b, c);
-
-	            if (occurrencesA != occurrencesB) {
-	                return false;
-	            }
-	        }
-
-	        return true;
-	    }
-
-	    private int countOccurrences(String s, char c) {
-	        int count = 0;
-	        for (int i = 0; i < s.length(); i++) {
-	            if (Character.isLetter(s.charAt(i)) && s.charAt(i) == c) {
-	                count++;
-	            }
-	        }
-	        return count;
+	private int countOccurrences(String a, char current) {
+		int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            current = a.charAt(i);
+            if(Character.isLetter(current)){
+            	count++;
+            }
+        }
+		return count;
 	}
 
 	
