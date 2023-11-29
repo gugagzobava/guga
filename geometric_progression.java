45d376afdfc958c552fad3fb5fcee55a9e5f0288
S <s>
2023-11-29 10:07:13 +0400

    {"types":"C","runs":4}

--- geometric_progression.java
+++ geometric_progression.java
@@ -3,34 +3,35 @@ import acm.program.ConsoleProgram;
 public class geometric_progression extends ConsoleProgram{
 	public void run(){
 		
-		        int b1 = readInt("Enter the first term (or -1 to exit): ");
-		        if (b1 == -1) {
-		            println("You entered -1 without providing initial terms. Exiting.");
-		            return;
-		        }
+	        int b1 = readInt("Enter the first term (or -1 to exit): ");
+	        if (b1 == -1) {
+	            println("You entered -1 without providing initial terms. Exiting.");
+	            return;
+	        }
 
-		        int b2 = readInt("Enter the second term: ");
+	        int b2 = readInt("Enter the second term: ");
 
-		        // Infer the common ratio (q) based on the first two terms
-		        int q = (b2 != 0) ? b2 / b1 : 0;
+	        // Infer the common ratio (q) based on the first two terms
+	        int q = (b2 != 0) ? b2 / b1 : 0;
 
-		        while (b2 != -1) {
-		            if (q == 0) {
-		                println("The common ratio cannot be zero. Exiting.");
-		                return;
-		            }
+	        while (b2 != -1) {
+	            if (q == 0) {
+	                println("The common ratio cannot be zero. Exiting.");
+	                return;
+	            }
 
-		            if (b2 != b1 * q) {
-		                println("Not a geometric progression.");
-		                return;
-		            }
+	            if (b2 != b1 * q) {
+	                println("Not a geometric progression.");
+	                return;
+	            }
 
-		            b1 = b2;
-		            b2 = readInt("Enter the next term (or -1 to exit): ");
-		        }
+	            b1 = b2;
+	            b2 = readInt("Enter the next term (or -1 to exit): ");
+	        }
 
-		        println("You entered -1. Exiting.");
-		    }
+	        // Check for geometric progression when -1 is entered
+	        println("It is a geometric progression.");
+	    }
 	
 //		int b1 = readInt();
 //		int b2 = readInt();
