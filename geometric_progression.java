b9b5c9b184154b5c7604b9e15cdc8a95709b8df7
S <s>
2023-11-29 10:02:20 +0400

    {"types":"C","runs":2}

--- geometric_progression.java
+++ geometric_progression.java
@@ -5,14 +5,25 @@ public class geometric_progression extends ConsoleProgram{
 		int b1 = readInt();
 		int b2 = readInt();
 		while(b1 != -1){
-			b1 = b2;		
+			 b1 = readInt("Enter the first term (b1): ");
+	            b2 = readInt("Enter the second term (b2): ");
+	        }
+
+	        int q = 0; // Initialize q with some value (you might want to use readInt to get q)
+	        if (q == b2 / b1) {
+	            println("Geometric progression");
+	        } else {
+	            println("Not a geometric progression");
+	        }
 		}
-		
-		int q;
-		if(q == b2 / b1){
-			println("progresia");
-		}
-	}
-	
+//			b1 = b2;		
+//		}
+//		
+//		int q;
+//		if(q == b2 / b1){
+//			println("progresia");
+//		}
+//	}
+//	
 
 }
