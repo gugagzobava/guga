d98ac70c9d05ca3187a8917df2ca48a9fd1267bb
S <s>
2023-11-29 09:58:23 +0400

    {"types":"C","runs":1}

--- geometric_progression.java
+++ geometric_progression.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class geometric_progression {
+public class geometric_progression extends ConsoleProgram{
+	public void run(){
+		int b1 = readInt();
+		int b2 = readInt();
+		while(b1 != -1){
+			b1 = b2;		
+		}
+		
+		int q;
+		if(q == b2 / b1){
+			println("progresia");
+		}
+	}
+	
 
 }
