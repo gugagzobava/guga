12d4504182d9fd3ceb24707ff8bc333b5af0e845
S <s>
2023-11-29 10:05:54 +0400

    {"types":"C","runs":3}

--- geometric_progression.java
+++ geometric_progression.java
@@ -2,20 +2,39 @@ import acm.program.ConsoleProgram;
 
 public class geometric_progression extends ConsoleProgram{
 	public void run(){
-		int b1 = readInt();
-		int b2 = readInt();
-		while(b1 != -1){
-			 b1 = readInt("Enter the first term (b1): ");
-	            b2 = readInt("Enter the second term (b2): ");
-	        }
+		
+		        int b1 = readInt("Enter the first term (or -1 to exit): ");
+		        if (b1 == -1) {
+		            println("You entered -1 without providing initial terms. Exiting.");
+		            return;
+		        }
 
-	        int q = 0; // Initialize q with some value (you might want to use readInt to get q)
-	        if (q == b2 / b1) {
-	            println("Geometric progression");
-	        } else {
-	            println("Not a geometric progression");
-	        }
-		}
+		        int b2 = readInt("Enter the second term: ");
+
+		        // Infer the common ratio (q) based on the first two terms
+		        int q = (b2 != 0) ? b2 / b1 : 0;
+
+		        while (b2 != -1) {
+		            if (q == 0) {
+		                println("The common ratio cannot be zero. Exiting.");
+		                return;
+		            }
+
+		            if (b2 != b1 * q) {
+		                println("Not a geometric progression.");
+		                return;
+		            }
+
+		            b1 = b2;
+		            b2 = readInt("Enter the next term (or -1 to exit): ");
+		        }
+
+		        println("You entered -1. Exiting.");
+		    }
+	
+//		int b1 = readInt();
+//		int b2 = readInt();
+//		while(b1 != -1){
 //			b1 = b2;		
 //		}
 //		
