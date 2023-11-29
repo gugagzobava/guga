89b864aab3033bf412cbbafd39c11672c6830050
S <s>
2023-11-30 01:18:13 +0400

    {"types":"C","runs":27}

--- midterm_practice_5.java
+++ midterm_practice_5.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class midterm_practice_5 extends ConsoleProgram{
 	public void run(){
 //		
-		  String a = "mama mia";
+		  String a = "mama saa";
 	        String b = "mimaiam";
 	        println(anagrams(a, b));
 	    }
