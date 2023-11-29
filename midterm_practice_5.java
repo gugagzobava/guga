f8f73bd5f96cbb7431127ebb0d6a59b4d2be14c3
S <s>
2023-11-30 00:46:46 +0400

    {"types":"C","runs":12}

--- midterm_practice_5.java
+++ midterm_practice_5.java
@@ -1,4 +1,42 @@
+import acm.program.ConsoleProgram;
 
-public class midterm_practice_5 {
+public class midterm_practice_5 extends ConsoleProgram{
+	public void run(){
+		String a = "mama mia";
+		String b = "mimaiam";
+		println(anagrams(a, b));
+		
+			
+		
+	}
+
+	private boolean anagrams(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();		
+		for(int i = 0; i < a.length(); i++){
+			char current = a.charAt(i);
+			 if (Character.isLetter(current)) {
+	                int occurrencesA = countOccurrences(a, current);
+	                int occurrencesB = countOccurrences(b, current);
+	                if (occurrencesA != occurrencesB) {
+	                    return false;
+	                }
+	            }
+		}
+		return true;
+	}
+
+	private int countOccurrences(String a, char current) {
+		int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            current = a.charAt(i);
+            if(Character.isLetter(current)){
+            	count++;
+            }
+        }
+		return count;
+	}
+
+	
 
 }
