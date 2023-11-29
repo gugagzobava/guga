a78e4a44628ada29eb8340106e05d0259a0e5594
S <s>
2023-11-30 00:10:55 +0400

    {"types":"C","runs":6}

--- geometric_progression.java
+++ geometric_progression.java
@@ -2,31 +2,25 @@ import acm.program.ConsoleProgram;
 
 public class geometric_progression extends ConsoleProgram {
 	public void run() {
-int b1 = readInt("Enter the first term (or -1 to exit): ");
-        
-        if (b1 == -1) {
-            println("You need to enter at least two terms.");
-            return;
-        }
-
-        int b2 = readInt("Enter the second term: ");
-
-        int q = b2 / b1;
-
-        while (true) {
-            if (b2 == -1) {
-                println("It is a geometric progression.");
-                break;
-            }
-
-            if (b2 != b1 * q) {
-                println("Not a geometric progression.");
-                break;
-            }
-
-            b1 = b2;
-            b2 = readInt("Enter the next term (or -1 to finish): ");
-        }
+		int b1 = readInt();
+		int b2 = readInt();
+		int q = b2 / b1;
+		boolean check = true;
+		while(true){
+			b1 = b2;
+			b2 = readInt();
+			if(b2 != b1 * q){
+				check = false;
+			}
+			if(b2 == -1){
+				break;
+			}
+		}
+		if (check = true){
+			println("progresia");
+		} else {
+			println("ara");
+		}
 //		int b1 = readInt();
 //		if (b1 == -1) {
 //			
