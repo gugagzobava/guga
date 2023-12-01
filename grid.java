c117a07673f2463be3894ad65bc88fdd0f610340
S <s>
2023-12-01 16:01:07 +0400

    {"types":"C","runs":24}

--- grid.java
+++ grid.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
@@ -9,16 +10,43 @@ public class grid extends GraphicsProgram {
 	private int NUM_ROWS = 5;
 	private int NUM_COLOMNS = 5;
 	private GRect firstClicked = null;
-    private GRect secondClicked = null;
+	private GRect secondClicked = null;
 
 	public void run() {
 		drawGrid();
-		
+		addMouseListeners();
+
+	}
+
+	public void MouseClicked(MouseEvent e) {
+		GRect obj = (GRect) getElementAt(e.getX(), e.getY());
+		if (obj == null) {
+			return;
+		}
+		if (firstClicked == null) {
+			firstClicked = obj;
+		} else if (secondClicked == null) {
+			secondClicked = obj;
+		}
+		Color firstRectColor = firstClicked.getColor();
+        Color firstRectFillColor = firstClicked.getFillColor();
+
+        Color secondRectColor = secondClicked.getColor();
+        Color secondRectFillColor = secondClicked.getFillColor();
+
+        if (firstRectColor.equals(secondRectColor) && firstRectFillColor.equals(secondRectFillColor)) {
+            remove(firstClicked);
+            remove(secondClicked);
+        }
+
+        // Clear selections for the next click
+        firstClicked = null;
+        secondClicked = null;
 	}
 
 	private void drawGrid() {
 		double x = getWidth() / NUM_COLOMNS;
-		double y = getHeight() / NUM_ROWS;	
+		double y = getHeight() / NUM_ROWS;
 		for (int rows = 0; rows < NUM_ROWS; rows++) {
 			for (int colomns = 0; colomns < NUM_COLOMNS; colomns++) {
 				GRect rect = new GRect(x, y);
@@ -28,18 +56,18 @@ public class grid extends GraphicsProgram {
 				rect.setFilled(true);
 				rect.setFillColor(borderColor);
 				add(rect, colomns * x, rows * y);
-			
+
 			}
 		}
-		
+
 	}
 
 	private Color getRandomColor() {
 		int randomColor = rgen.nextInt(0, 2);
-		if(randomColor == 0){
+		if (randomColor == 0) {
 			return Color.GREEN;
 		}
-		if(randomColor == 1){
+		if (randomColor == 1) {
 			return Color.BLUE;
 		}
 		return Color.YELLOW;
