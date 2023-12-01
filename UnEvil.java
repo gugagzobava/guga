b5243adae5a3d26e4dbe0dea1d82292f4563dd7c
S <s>
2023-12-01 19:31:32 +0400

    {"types":"C","runs":101}

--- UnEvil.java
+++ UnEvil.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class UnEvil {
+public class UnEvil extends ConsoleProgram{
+	public void run(){
+		String s = "how arxxe yommu?";
+		println(unEvilText(s));
+		
+	}
+
+	private String unEvilText(String s) {
+		String result = "";
+		for(int i = 0; i < s.length(); i++){
+			char current = s.charAt(i);
+			if(i < s.length() - 1 && current == s.charAt(i + 1)){
+				result = s.substring(0, i) + s.substring(i+1);
+			}
+		}
+		return result;
+	}
 
 }
