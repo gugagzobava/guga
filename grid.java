b9bac6d99ff7e0b2dc63d56aed64006409ada8c6
S <s>
2023-12-01 15:18:01 +0400

    {"types":"C","runs":9}

--- grid.java
+++ grid.java
@@ -8,7 +8,12 @@ public class grid extends GraphicsProgram {
 		int x = getWidth() / NUM_COLOUMNS;
 		int y = getHeight() / NUM_ROWS;
 		GRect rect = new GRect(x, y);
-		add(rect);
+		for(int rows = 0; rows < NUM_ROWS; rows++){
+			for (int coloumns = 0; coloumns < NUM_COLOUMNS; coloumns++){
+				add(rect, rows * x, coloumns * y);
+				
+			}
+		}
 	}
 
 }
