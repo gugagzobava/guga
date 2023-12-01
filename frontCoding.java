b94433ed49478061e1b9dc2e74a919611b156d8a
S <s>
2023-12-01 16:37:40 +0400

    {"types":"C","runs":37}

--- frontCoding.java
+++ frontCoding.java
@@ -7,19 +7,21 @@ public class frontCoding extends ConsoleProgram {
 		int count = 0;
 		int counta = a.length() - count;
 		int countb = b.length() - count;
+		String remainingA = a.substring(count - 1);
+		String remainingB = b.substring(count - 1);
 		println(FrontCoding(a, b, count));
 
 	}
 
 	private String FrontCoding(String a, String b, int count) {
 		String result = "";
-		
 		for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
 			char currentA = a.charAt(i);
 			char currentB = b.charAt(i);
 			if (currentA == currentB) {
 				result += currentA;
 				count++;
+				
 			} else {
 				break;
 
