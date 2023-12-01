fc54a5156bb3f7cc4d273ae82f557af25f5f3aa9
S <s>
2023-12-01 18:02:41 +0400

    {"types":"C","runs":63}

--- wordShrugg.java
+++ wordShrugg.java
@@ -4,22 +4,23 @@ public class wordShrugg extends ConsoleProgram {
 	public void run() {
 		String s = readLine();
 		longestRunCompression(s);
-		
+
 	}
 
 	private void longestRunCompression(String s) {
 		int count = 1;
-		for(int i = 0; i < s.length(); i++){
+		for (int i = 0; i < s.length(); i++) {
 			char current = s.charAt(i);
-			if(current == s.charAt(i+1)){
+			if (current == s.charAt(i + 1)) {
 				count++;
-			} else{
-			print(count);
-            print(current);
-            count = 1;
+			} else {
+				print(count);
+				print(current);
+				count = 1;
+			}
 		}
-		}
-		println();
+		 print(count);
+	        print(s.charAt(s.length() - 1));
 	}
 
 }
