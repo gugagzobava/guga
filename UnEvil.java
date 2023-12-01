e28eca6a92e1b05e08118b6a2788ee1a9102ea61
S <s>
2023-12-01 20:05:11 +0400

    {"types":"C","runs":124}

--- UnEvil.java
+++ UnEvil.java
@@ -6,35 +6,18 @@ public class UnEvil extends ConsoleProgram {
 		println(unEvilText(s));
 
 	}
-	private String unEvilText(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (i < s.length() - 1 && current == s.charAt(i + 1)) {
-                // Skip the next character
-                i++;
-            } else {
-                result += current;
-            }
-        }
-        return result;
-    }
 
-//	private String unEvilText(String s) {
-//		String result = "";
-//		for (int i = 0; i < s.length(); i++) {
-//			char current = s.charAt(i);
-//			boolean isDuplicate = false;
-//			while (i < s.length() - 1 && current == s.charAt(i + 1)) {
-//				isDuplicate = true;
-//				i++;
-//			}
-//			if (!isDuplicate) {
-//				result += current;
-//			}
-//
-//		}
-//		return result;
-//	}
+	private String unEvilText(String s) {
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			char current = s.charAt(i);
+			if (i < s.length() - 1 && current == s.charAt(i + 1)) {
+				i++;
+			} else {
+				result += current;
+			}
+		}
+		return result;
+	}
 
 }
