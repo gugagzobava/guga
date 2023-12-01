9045df55d9119e38eac62ad81961e32e55671697
S <s>
2023-12-01 15:20:53 +0400

    {"types":"C","runs":10}

--- grid.java
+++ grid.java
@@ -3,15 +3,16 @@ import acm.program.GraphicsProgram;
 
 public class grid extends GraphicsProgram {
 	private int NUM_ROWS = 5;
-	private int NUM_COLOUMNS = 5; 
+	private int NUM_COLOMNS = 5;
+
 	public void run() {
-		int x = getWidth() / NUM_COLOUMNS;
-		int y = getHeight() / NUM_ROWS;
-		GRect rect = new GRect(x, y);
-		for(int rows = 0; rows < NUM_ROWS; rows++){
-			for (int coloumns = 0; coloumns < NUM_COLOUMNS; coloumns++){
-				add(rect, rows * x, coloumns * y);
-				
+		double x = getWidth() / NUM_COLOMNS;
+		double y = getHeight() / NUM_ROWS;
+		for (int rows = 0; rows < NUM_ROWS; rows++) {
+			for (int colomns = 0; colomns < NUM_COLOMNS; colomns++) {
+				GRect rect = new GRect(x, y);
+				add(rect, rows * x, colomns * y);
+
 			}
 		}
 	}
