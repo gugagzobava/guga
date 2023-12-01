39b1c38d35f96942f6d5bc25d44876121c1559cd
S <s>
2023-12-01 19:49:35 +0400

    {"types":"C","runs":114}

--- UnEvil.java
+++ UnEvil.java
@@ -2,40 +2,26 @@ import acm.program.ConsoleProgram;
 
 public class UnEvil extends ConsoleProgram {
 	public void run() {
-		String s = "how arxxe yommunn?";
+		String s = "how arxxe yommmmmmunn?";
 		println(unEvilText(s));
 
 	}
+
 	private String unEvilText(String s) {
-        String result = "";
-        s = s.toLowerCase(); // Convert to lowercase for case-insensitivity
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			char current = s.charAt(i);
+			boolean isDuplicate = false;
+			while (i < s.length() - 1 && current == s.charAt(i + 1)) {
+				isDuplicate = true;
+				i++;
+			}
+			if (!isDuplicate) {
+				result += current;
+			}
 
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-
-            // Check if the current character is different from the next one
-            if (i == s.length() - 1 || current != s.charAt(i + 1)) {
-                result += current;
-            }
-        }
-
-        return result;
-    }
-//	private String unEvilText(String s) {
-//		String result = "";
-//		for (int i = 0; i < s.length(); i++) {
-//			char current = s.charAt(i);
-//			boolean isDuplicate = false;
-//			while (i < s.length() - 1 && current == s.charAt(i + 1)) {
-//				isDuplicate = true;
-//				i++;
-//			}
-//			if (!isDuplicate) {
-//				result += current;
-//			}
-//
-//		}
-//		return result;
-//	}
+		}
+		return result;
+	}
 
 }
