bd10f389b2ab7e4fb059f3b617f6342f157cc3d8
S <s>
2023-12-01 19:56:15 +0400

    {"types":"C","runs":116}

--- UnEvil.java
+++ UnEvil.java
@@ -9,14 +9,16 @@ public class UnEvil extends ConsoleProgram {
 
 	private String unEvilText(String s) {
 		String result = "";
+		int count = 0;
 		for (int i = 0; i < s.length(); i++) {
 			char current = s.charAt(i);
 			boolean isDuplicate = false;
 			while (i < s.length() - 1 && current == s.charAt(i + 1)) {
 				isDuplicate = true;
 				i++;
+				count++;
 			}
-			if (!isDuplicate) {
+			if (!isDuplicate || count <= 2) {
 				result += current;
 			}
 
