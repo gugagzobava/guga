8024b2f1b27892f10590f305aa1d090f2c06e2c9
S <s>
2023-12-01 18:12:19 +0400

    {"types":"C","runs":67}

--- wordShrugg.java
+++ wordShrugg.java
@@ -1,26 +1,28 @@
 import acm.program.ConsoleProgram;
 
 public class wordShrugg extends ConsoleProgram {
-	public void run() {
-		String s = readLine();
-		longestRunCompression(s);
+    public void run() {
+        String s = readLine();
+        longestRunCompression(s);
+        println(); // Print a newline at the end
+    }
 
-	}
+    private void longestRunCompression(String s) {
+        int count = 1;
 
-	private void longestRunCompression(String s) {
-		int count = 1;
-		for (int i = 0; i < s.length(); i++) {
-			char current = s.charAt(i);
-			if (current == s.charAt(i - 1)) {
-				count++;
-			}
-			println(count + current);
-			count = 1;
-			
-		}
-		print(count);
-		print(s.charAt(s.length() - 1));
-
-	}
-
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (i < s.length() - 1 && current == s.charAt(i + 1)) {
+                // If the current character is the same as the next one, increment the count
+                count++;
+            } else {
+                // If a new character is encountered or the end of the string is reached,
+                // print the count and the character
+                System.out.print(count);
+                System.out.print(current);
+                // Reset the count for the next character
+                count = 1;
+            }
+        }
+    }
 }
