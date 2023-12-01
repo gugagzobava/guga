89077dd634297082acafeaa8f059ebe497e0dea3
S <s>
2023-12-01 23:33:17 +0400

    {"types":"C","runs":12}

--- partOfEachOther.java
+++ partOfEachOther.java
@@ -1,4 +1,38 @@
+import acm.program.ConsoleProgram;
 
-public class partOfEachOther {
+public class partOfEachOther extends ConsoleProgram {
+	public void run() {
+		String a = "mama ";
+		String b = "mimaiam";
+		println(anagrams(a, b));
+
+	}
+
+	private boolean anagrams(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+		for (int i = 0; i < a.length(); i++) {
+			char current = a.charAt(i);
+			if (Character.isLetter(current)) {
+				int occurrencesA = countOccurrences(a, current);
+				int occurrencesB = countOccurrences(b, current);
+				if (occurrencesA != occurrencesB) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private int countOccurrences(String a, char current) {
+		int count = 0;
+		for (int i = 0; i < a.length(); i++) {
+			current = a.charAt(i);
+			if (Character.isLetter(current)) {
+				count++;
+			}
+		}
+		return count;
+	}
 
 }
