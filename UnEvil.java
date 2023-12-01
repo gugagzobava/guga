dbe72ad7699430f7721f9dbfaea5215dce1c157d
S <s>
2023-12-01 19:40:07 +0400

    {"types":"C","runs":108}

--- UnEvil.java
+++ UnEvil.java
@@ -12,7 +12,7 @@ public class UnEvil extends ConsoleProgram{
 		for(int i = 0; i < s.length(); i++){
 			char current = s.charAt(i);
 			 if (i == s.length() - 1 || current != s.charAt(i + 1)) {
-	                result += current;
+	                result = result + current;
 	            }
 //			if(i < s.length() - 1 && current == s.charAt(i + 1)){
 //				result = s.substring(0, i) + s.substring(i+2);
