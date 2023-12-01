027b2f257fbbfcf2b90ff5204a964dd5f9abd1eb
S <s>
2023-12-01 14:46:29 +0400

    {"types":"C","runs":1}

--- dogs.java
+++ dogs.java
@@ -1,11 +1,13 @@
 import acm.program.ConsoleProgram;
 
 public class dogs extends ConsoleProgram{
-	private String name = "";
 	public void run (){
 		println("Welcome to grand opening of the new Freeuni dog park!");
 		while(true){
-			name = readLine("What is your dog's name ?");
+			String name = readLine("What is your dog's name ?");
+			if(name == ""){
+				break;
+			}
 			int size = readInt("How big is your dog? Enter a number; 1 - small, 2 - medium, 3 - large ");
 			int candy = 2 * size;
 			println("Oh boy! " + name + " gets " + candy + "dog treats." );
