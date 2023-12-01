831a87f1a7a17fb1e7363311632d4340a88b1833
S <s>
2023-12-01 18:09:42 +0400

    {"types":"C","runs":66}

--- wordShrugg.java
+++ wordShrugg.java
@@ -11,7 +11,7 @@ public class wordShrugg extends ConsoleProgram {
 		int count = 1;
 		for (int i = 0; i < s.length(); i++) {
 			char current = s.charAt(i);
-			if (i <= s.length() - 1 && current == s.charAt(i + 1)) {
+			if (current == s.charAt(i - 1)) {
 				count++;
 			}
 			println(count + current);
