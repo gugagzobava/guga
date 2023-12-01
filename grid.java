42e724c57d418138629e099a91cd6572b9e079b7
S <s>
2023-12-01 15:11:16 +0400

    {"types":"C","runs":7}

--- grid.java
+++ grid.java
@@ -1,4 +1,12 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class grid {
+public class grid extends GraphicsProgram {
+	private int NUM_ROWS = 5;
+	private int NUM_COLOUMNS = 5; 
+	public void run() {
+		GRect rect = new GRect(getWidth() / NUM_COLOUMNS, getHeight() / NUM_ROWS);
+		add(rect);
+	}
 
 }
