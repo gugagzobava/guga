b2333fb2928128ec9a04d6d273a68ad0b832758d
S <s>
2023-12-01 19:43:20 +0400

    {"types":"C","runs":110}

--- UnEvil.java
+++ UnEvil.java
@@ -1,29 +1,25 @@
 import acm.program.ConsoleProgram;
 
-public class UnEvil extends ConsoleProgram{
-	public void run(){
-		String s = "how arxxe yommu?";
+public class UnEvil extends ConsoleProgram {
+	public void run() {
+		String s = "how arxxe yommunn?";
 		println(unEvilText(s));
-		
+
 	}
 
 	private String unEvilText(String s) {
 		String result = "";
-		for(int i = 0; i < s.length(); i++){
+		for (int i = 0; i < s.length(); i++) {
 			char current = s.charAt(i);
 			boolean isDuplicate = false;
-while(i < s.length() - 1 && current == s.charAt(i + 1)){
-    isDuplicate = true;
-    i++;
-}
-if (!isDuplicate) {
-    result += current;
-}
-	
+			while (i < s.length() - 1 && current == s.charAt(i + 1)) {
+				isDuplicate = true;
+				i++;
+			}
+			if (!isDuplicate) {
+				result += current;
+			}
 
-//			if(i < s.length() - 1 && current == s.charAt(i + 1)){
-//				result = s.substring(0, i) + s.substring(i+2);
-//			}
 		}
 		return result;
 	}
