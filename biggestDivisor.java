ef54f6f887f12216afa657793dafb8c67c71609f
S <s>
2023-12-01 23:02:07 +0400

    {"types":"C","runs":1}

--- biggestDivisor.java
+++ biggestDivisor.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class biggestDivisor {
+public class biggestDivisor extends ConsoleProgram{
+	public void run(){
+		int n = readInt("enter a number: ");
+		println(largestPrimeDivisor(n));
+		
+	}
+
+	private int largestPrimeDivisor(int n) {
+		for(int i = 2; i <= n; i++){
+			if(n % i ==0){
+				return i;
+			}
+		}
+		return n;
+	}
 
 }
