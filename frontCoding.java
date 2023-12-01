afbd6c5891e45078db5371dac4d25256b750285b
S <s>
2023-12-01 16:28:51 +0400

    {"types":"C","runs":32}

--- frontCoding.java
+++ frontCoding.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class frontCoding {
+public class frontCoding extends ConsoleProgram {
+	public void run() {
+		String a = "million";
+		String b = "millenia";
+		FrontCoding(a, b);
 
+	}
+
+	private String FrontCoding(String a, String b) {
+		prefix(a, b);
+	}
+
+	private String prefix(String a, String b) {
+		String result = "";
+		int count = 0;
+		for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+			char currentA = a.charAt(i);
+			char currentB = b.charAt(i);
+			if (currentA == currentB) {
+				result += currentA;
+				count++;
+			} else {
+				break;
+
+			}
+
+		}
+		return result;
+	}
 }
