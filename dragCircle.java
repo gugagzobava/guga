91cb23cec78e4d154233f0102d1e9f8a7f965cda
S <s>
2023-12-02 11:08:02 +0400

    {"types":"C","runs":1}

--- dragCircle.java
+++ dragCircle.java
@@ -1,4 +1,16 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class dragCircle {
+public class dragCircle extends GraphicsProgram {
+	private static final int RADIUS = 100;
+	private int r = RADIUS;
+	private GOval ball;
+	public void run(){
+		addMouseListeners();
+		ball = new GOval(getWidth() / 2, getHeight() / 2, 2 * r, 2 * r);
+		add(ball);
+		
+		
+	}
 
 }
