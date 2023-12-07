9e5ee94f3f720324c26a52a313fb8734d4f5dc83
S <s>
2023-12-07 10:12:27 +0400

    {"types":"C","runs":3}

--- problem54.java
+++ problem54.java
@@ -3,11 +3,20 @@ import acm.program.ConsoleProgram;
 public class problem54 extends ConsoleProgram{
 	public void run() {
 		int n = readInt("Enter amount of numbers: ");
-		int[] numbers = new int[n];
-
+		int[] curr = new int[n];
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-        	numbers[i] = readInt();
+        	curr[i] = readInt();
+        	if(curr[i] > largest){
+        		largest = secondLargest;
+        		curr[i] = largest;     		
+        	}
+        	if(curr[i] > secondLargest && curr[i] < largest){
+        		curr[i] = secondLargest;
+        	}
         }
+        println(largest + "and" + secondLargest + "are biggest numbers");
 	}
 
 }
