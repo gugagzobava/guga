1ffc596d06adde8e87db8df1ff7b842e805bb01e
S <s>
2023-12-12 11:51:59 +0400

    {"types":"C","runs":2}

--- problem58.java
+++ problem58.java
@@ -1,4 +1,30 @@
+import java.lang.reflect.Array;
 
-public class problem58 {
+import acm.program.ConsoleProgram;
 
+public class problem58 extends ConsoleProgram {
+	public void run(){
+		int[][] array = new int [5][5];
+		magicMatrix(array);
+		
+	}
+	private boolean magicMatrix(int[][] array) {
+		for(int i = 0; i < array.length; i++){
+			if(array.length != array[i].length){
+				return false;
+			}
+		}
+		int n = array.length;
+		boolean[] seen = new boolean [n * n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(array[i][j] < 1 || array[i][j] > n * n || seen[array[i][j] - 1]){
+					return false;
+				}
+				seen[array[i][j] - 1] = true;
+			}
+		}
+		return true;
+		
+	}
 }
