82b322814c3281d0d1554805cc1b6596fc734d66
S <s>
2023-12-12 12:10:01 +0400

    {"types":"C","runs":2}

--- problem59.java
+++ problem59.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class problem59 {
+public class problem59 extends ConsoleProgram{
+	public void run(){
+		int[][] array = new int [5][7];
+	}
+	private boolean isFilled(int[][] array, int startX, int startY, int x, int y) {
+		for(int i = startX; i < startX + x; i++){
+			for (int j = startY; j < startY + y; j++){
+				if(array[i][j] == 0){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
 }
