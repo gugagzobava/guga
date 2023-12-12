eec84e4f7e16dd545aadc0795c573312e67d08c7
S <s>
2023-12-12 11:25:26 +0400

    {"types":"C","runs":1}

--- arrayexamples.java
+++ arrayexamples.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class arrayexamples {
+public class arrayexamples extends ConsoleProgram{
+	public void run(){
+		int[][] a = new int[10][10];
+		int[][] b =new int [5][5];
+		println("gaysex");
+	}
 
 }
