6a1df0a28aef980d3c9eae9b3baa8fb8209e2f41
S <s>
2023-12-12 11:28:00 +0400

    {"types":"C","runs":2}

--- arrayexamples.java
+++ arrayexamples.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class arrayexamples extends ConsoleProgram{
 	public void run(){
-		int[][] a = new int[10][10];
-		int[][] b =new int [5][5];
-		println("gaysex");
+		String[][] a = new String[10][10];
+		a[3][4] = "HELLO"; 
+		println(Arrays.deepToString(a));
 	}
 
 }
