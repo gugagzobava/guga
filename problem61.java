1ffc84fe041e976023754ce3494da9e97100f5aa
S <s>
2023-12-15 13:08:25 +0400

    {"types":"C","runs":6}

--- problem61.java
+++ problem61.java
@@ -3,12 +3,26 @@ import java.util.HashMap;
 import acm.program.ConsoleProgram;
 
 public class problem61 extends ConsoleProgram {
-	public void run(){
-		HashMap <Integer,Integer> hashMap = new HashMap<Integer,Integer>();
+	public void run() {
+	//	HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 		int m = readInt();
 		int n = readInt();
-		for(int i = 0; i < n; i++){
-			int numbers = readInt();
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		boolean found = false;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (array[i] == m - array[i]) {
+					found = true;
+					println(i + "" + j);
+					break;
+				}
+			}
+		}
+		if(!found){
+			println("Not found");
 		}
 	}
 
