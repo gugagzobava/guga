0e6b1e3aed406b7a54016bfce5dcfc89e2e10233
S <s>
2023-12-15 12:34:49 +0400

    {"types":"C","runs":1}

--- hashampExamples.java
+++ hashampExamples.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class hashampExamples {
+import acm.program.ConsoleProgram;
+
+public class hashampExamples extends ConsoleProgram{
+	public void run(){
+		HashMap <String,Integer> hashMap = new HashMap<String,Integer>();
+		ArrayList <Integer> arrayList = new ArrayList<Integer>();
+		hashMap.put("ABDD", 2000);
+		hashMap.put("ABC", 3000);
+		println(hashMap.size());
+		
+	}
 
 }
