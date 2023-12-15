7501bd81737367a16eabc00a3bdac14fa98181ca
S <s>
2023-12-15 13:03:09 +0400

    {"types":"C","runs":4}

--- hashampExamples.java
+++ hashampExamples.java
@@ -9,10 +9,18 @@ public class hashampExamples extends ConsoleProgram{
 		ArrayList <Integer> arrayList = new ArrayList<Integer>();
 		hashMap.put("ABDD", 2000);
 		hashMap.put("ABC", 3000);
+		if(!hashMap.containsKey(key)){
+			hashMap.put(key, value);
+		}
+		hashMap.putIfAbsent(key, value);
+		
+		hashMap.getOrDefault(key, 0);
+		hashMap.get(key);
+		
 		for (String x : hashMap.keySet()){
 			println(x + "" + hashMap.get(x));
 		}
-		println(hashMap.size());
+//		println(hashMap.size());
 		println(hashMap.toString());
 	}
 
--- problem61.java
+++ problem61.java
@@ -1,4 +1,15 @@
+import java.util.HashMap;
 
-public class problem61 {
+import acm.program.ConsoleProgram;
+
+public class problem61 extends ConsoleProgram {
+	public void run(){
+		HashMap <Integer,Integer> hashMap = new HashMap<Integer,Integer>();
+		int m = readInt();
+		int n = readInt();
+		for(int i = 0; i < n; i++){
+			n = readInt();
+		}
+	}
 
 }
