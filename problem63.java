efd3b1aff87a45226ae5e134dc85b5813de2f7a8
S <s>
2023-12-19 12:01:41 +0400

    {"types":"C","runs":2}

--- problem63.java
+++ problem63.java
@@ -1,4 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
-public class problem63 {
+import acm.program.ConsoleProgram;
+
+public class problem63 extends ConsoleProgram {
+	public void run() {
+		HashMap<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
+
+		while (true) {
+			String line = readLine();
+			if (line.isEmpty()) {
+				break;
+			}
+			StringTokenizer tokenizer = new StringTokenizer(readLine());
+			String a = tokenizer.nextToken();
+			String b = tokenizer.nextToken();
+			map.putIfAbsent(b, new HashSet<String>());
+			map.get(b).add(a);
+		}
+		String name = null;
+		int ans = 0;
+		for(String friend : map.keySet()){
+			int amount = map.get(friend).size();
+			if(amount > ans){
+				ans = amount;
+				name = friend;
+			}
+		}
+		println(name);
+		println(map);
+	}
 
 }
