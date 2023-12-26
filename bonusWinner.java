59f58ff03ff8a8910713f05661b942f220f6a683
S <s>
2023-12-26 11:41:48 +0400

    {"types":"C","runs":0}

--- bonusWinner.java
+++ bonusWinner.java
@@ -3,19 +3,26 @@ import java.util.Map;
 
 import acm.program.ConsoleProgram;
 
-public class bonusWinner extends ConsoleProgram{
-	public void run(){
-		
+public class bonusWinner extends ConsoleProgram {
+	public void run() {
+
 	}
-	private String winner(Map <String, Integer> results){
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        	for(String result : results.values()){
-        		frequencyMap.put(result, frequencyMap.getOrDefault(results,  0) + 1);
-        			
-        		}
-        		
-        	
-		return null;
+
+	private String winner(Map<String, Integer> results) {
+		Map<Integer, Integer> frequencyMap = new HashMap<>();
+		for (int result : results.values()) {
+			frequencyMap.put(result, frequencyMap.getOrDefault(results, 0) + 1);
+		}
+		String winner = null;
+		for(String student : results.keySet()){
+			int result = results.get(student);
+			if(frequencyMap.get(result) == 1){
+				if(winner == null ||results.get(winner) > result){
+					winner = student;
+				}
+			}
+		}
+
+		return winner;
 	}
 }
-	
