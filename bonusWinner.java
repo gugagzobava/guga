a3bc406125fe65b49ef8e75d2c7e3feb706ee2aa
S <s>
2023-12-26 11:38:29 +0400

    {"types":"C","runs":0}

--- bonusWinner.java
+++ bonusWinner.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class bonusWinner {
+import acm.program.ConsoleProgram;
 
+public class bonusWinner extends ConsoleProgram{
+	public void run(){
+		
+	}
+	private String winner(Map <String, Integer> results){
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        	for(String result : results.values()){
+        		frequencyMap.put(result, frequencyMap.getOrDefault(results,  0) + 1);
+        			
+        		}
+        		
+        	
+		return null;
+	}
 }
+	
