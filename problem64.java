52667d916bf641d3215e91bc3c26e7b3d7a86fab
S <s>
2024-01-08 08:33:16 +0400

    {"types":"C","runs":2}

--- problem64.java
+++ problem64.java
@@ -1,4 +1,36 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
-public class problem64 {
+import acm.program.ConsoleProgram;
+
+public class problem64 extends ConsoleProgram {
+	public void run() {
+		HashMap<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
+
+		while (true) {
+			String line = readLine();
+			if (line.isEmpty()) {
+				break;
+			}
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			String a = tokenizer.nextToken();
+			String b = tokenizer.nextToken();
+			map.putIfAbsent(b, new HashSet<String>());
+			map.get(b).add(a);
+		}
+		String name = null;
+		int ans = 0;
+		for(String friend : map.keySet()){
+			int amount = map.get(friend).size();
+			if(amount > ans){
+				ans = amount;
+				name = friend;
+			}
+		}
+		println(name);
+		println(map);
+	}
 
 }
+
